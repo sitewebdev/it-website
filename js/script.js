d051810544f803edf930321b25c916fa0e62632e
pinpan-studio <58504240+pinpan-studio@users.noreply.github.com>
2022-06-12 21:32:34 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function(){
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel(
+        {
+            margin: 10,
+            nav: true,
+            navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        }
+    );
+
+    $('#article-action-left').click(function() { owl.trigger('prev.owl.carousel');})
+    $('#article-action-right').click(function() { owl.trigger('next.owl.carousel');})
+
+});
