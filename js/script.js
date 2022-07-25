b82ee3ffe1477adc54890aa3472553ae0970a5c7
pinpan-studio <58504240+pinpan-studio@users.noreply.github.com>
2022-07-25 10:06:23 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ $(document).ready(function(){
     $('#vid-action-left').click(function() { owl_vid.trigger('prev.owl.carousel');})
     $('#vid-action-right').click(function() { owl_vid.trigger('next.owl.carousel');})
 
-    //startWow();
+    startWow();
     try {
         $('[data-fancybox]').fancybox({
             youtube : {
@@ -212,23 +212,30 @@ function scrollToTop(){
 }
 
 function startWow() {
-    var wow = new WOW(
-        {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       true,       // trigger animations on mobile devices (default is true)
-          live:         true,       // act on asynchronously loaded content (default is true)
-          callback:     function(box) {
-            // the callback is fired every time an animation is started
-            // the argument that is passed in is the DOM node being animated
-          },
-          scrollContainer: null,    // optional scroll container selector, otherwise use window,
-          resetAnimation: true,     // reset animation on end (default is true)
-        }
-      );
-      wow.init();
-      console.log(wow)
+    try{
+        var wow = new WOW(
+            {
+              boxClass:     'wow',      // animated element css class (default is wow)
+              animateClass: 'animated', // animation css class (default is animated)
+              offset:       0,          // distance to the element when triggering the animation (default is 0)
+              mobile:       true,       // trigger animations on mobile devices (default is true)
+              live:         true,       // act on asynchronously loaded content (default is true)
+              callback:     function(box) {
+                console.log(box)
+                // the callback is fired every time an animation is started
+                // the argument that is passed in is the DOM node being animated
+              },
+              scrollContainer: null,    // optional scroll container selector, otherwise use window,
+              resetAnimation: true,     // reset animation on end (default is true)
+            }
+          );
+          wow.init();
+    }
+    catch(e){
+        console.log(e)
+        
+    }
+
 }
 
 
