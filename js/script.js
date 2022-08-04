ccc8def8691e1a98cf04379d008e6134c458a916
pinpan-studio <58504240+pinpan-studio@users.noreply.github.com>
2022-08-04 17:00:22 +0000

    avs

--- js/script.js
+++ js/script.js
@@ -132,19 +132,25 @@ $(document).ready(function(){
     loadDevTabContent();
 
 
-     
+
+try{
+    const ulx = document.getElementsByClassName("enjeux-secteur-title-bar");
+    ul = ulx[0];
+    console.log(ulx)
+    
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {stickyEnjeuxFunc()};
+    
+    
+    
+    // Get the offset position of the navbar
+    var sticky = ul.offsetTop;
+}
+catch(e){
+    console.log(e);
+}
 // Get the navbar
-const ulx = document.getElementsByClassName("enjeux-secteur-title-bar");
-ul = ulx[0];
-console.log(ulx)
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickyEnjeuxFunc()};
-
-
-
-// Get the offset position of the navbar
-var sticky = ul.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function stickyEnjeuxFunc() {
